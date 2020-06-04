036297033d2a1aa264b57e19d46656d621320273
Eyal Levy <elevyg91@gmail.com>
2020-06-04 11:55:36 -0400

    Frist deployment

---
+++ .history/server_20200604114504.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+
+// Security
+const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+
+// Middleware
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
+
+// Route Files
+const sectores = require('./routes/sector');
+const subsectores = require('./routes/subsector');
+const contadores = require('./routes/contador');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+// Initialize app
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+// File uploading
+app.use(fileupload());
+// Cookie parser
+app.use(cookieParser());
+// Sanitize data
+app.use(mongoSanitize());
+// Set security headers
+app.use(helmet());
+// Prevent XSS attacs
+app.use(xss());
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+});
+app.use(limiter);
+// Prevent http param pollution
+app.use(hpp());
+// Enable CORS
+app.use(cors());
+
+// Mount Routers
+app.use('/api/v1/sectores', sectores);
+app.use('/api/v1/subsectores', subsectores);
+app.use('/api/v1/contadores', contadores);
+
+// Error handler
+app.use(errorHandler);
+
+//  Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static.apply('client/build'));
+}
+
+// Initializing server
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV.underline} mode on port ${PORT}`
+      .yellow.bold
+  )
+);
+
+// Handle unhandled promise rejections
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
